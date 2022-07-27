17e9bd7cf211e3dc5bf3b54cae6266e9095e3639
al234790@uji.es <al234790@uji.es>
2022-07-27 21:40:13 +0200

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // Console.WriteLine("HOLA MUMDO!!!");
+            int num1 = 5;
+            int nuero2 = 6;
+            double num3 = 2.57;
+            double suma;
+            suma = num1 + num3;
+
+            Console.WriteLine(suma);
+            string Valor1 = Console.ReadLine();
+            Console.WriteLine("El numero introducido es:" + Valor1);
+            int valorint = Convert.ToInt32(Valor1);
+
+            Console.ReadKey();
+        }
+    }
+}
